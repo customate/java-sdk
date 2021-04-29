0f9866f5e1ca2d4bb38a3e80fa8a65e56df0324c
Savo <savobalac@yahoo.co.uk>
2021-04-29 15:52:50 +0100

    Write the test result to a file (so that the Jenkins job can check it).

--- TestNoAsync.js
+++ TestNoAsync.js
@@ -1,7 +1,4 @@
 const uri = process.argv.slice(3)[0];
 console.log('uri passed to JS file: ' + uri);
 const fs = require('fs');
-fs.writeFile('Output.txt', 'Success', (err) => {
-    // In case of a error throw err.
-    if (err) throw err;
-})
+fs.writeFile('Output.txt', 'Success')
